09eee7a9d0d4fcb04268ee8f3035bce19854d746
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2021-01-15 19:12:02 +0100

    Cache should be customer dependant

--- ps_newproducts.php
+++ ps_newproducts.php
@@ -259,4 +259,13 @@ class Ps_NewProducts extends Module implements WidgetInterface
 
         return $products_for_template;
     }
+
+    protected function getCacheId($name = null)
+    {
+        $cacheId = parent::getCacheId($name);
+        if(!empty($this->context->customer->id)){
+            $cacheId .= '|' . (int) $this->context->customer->id;
+        }
+        return $cacheId;
+    }
 }
