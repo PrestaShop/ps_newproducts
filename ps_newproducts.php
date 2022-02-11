5b338979665243e4e2831ddf142a25b1a2bf94f3
Progi1984 <franck.lefevre@prestashop.com>
2022-02-11 16:03:07 +0100

    Fixed PHPCSFixer

--- ps_newproducts.php
+++ ps_newproducts.php
@@ -24,11 +24,11 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 use PrestaShop\PrestaShop\Adapter\Image\ImageRetriever;
 use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
-use PrestaShop\PrestaShop\Core\Product\ProductListingPresenter;
 use PrestaShop\PrestaShop\Adapter\Product\ProductColorsRetriever;
+use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
+use PrestaShop\PrestaShop\Core\Product\ProductListingPresenter;
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -46,16 +46,16 @@ class Ps_NewProducts extends Module implements WidgetInterface
         $this->version = '1.0.1';
         $this->need_instance = 0;
 
-        $this->ps_versions_compliancy = array(
+        $this->ps_versions_compliancy = [
             'min' => '1.7.0.0',
             'max' => _PS_VERSION_,
-        );
+        ];
 
         $this->bootstrap = true;
         parent::__construct();
 
-        $this->displayName = $this->trans('New products block', array(), 'Modules.Newproducts.Admin');
-        $this->description = $this->trans('Highlight your store\'s newest products, display a block on the homepage and let the visitors know about your latest news.', array(), 'Modules.Newproducts.Admin');
+        $this->displayName = $this->trans('New products block', [], 'Modules.Newproducts.Admin');
+        $this->description = $this->trans('Highlight your store\'s newest products, display a block on the homepage and let the visitors know about your latest news.', [], 'Modules.Newproducts.Admin');
 
         $this->templateFile = 'module:ps_newproducts/views/templates/hook/ps_newproducts.tpl';
     }
@@ -109,63 +109,63 @@ class Ps_NewProducts extends Module implements WidgetInterface
     {
         $output = '';
         if (Tools::isSubmit('submitBlockNewProducts')) {
-
             $productNbr = Tools::getValue('NEW_PRODUCTS_NBR');
 
             if (!$productNbr || empty($productNbr)) {
                 $output .= $this->displayError(
-                    $this->trans('Please complete the "products to display" field.', array(), 'Modules.Newproducts.Admin' )
+                    $this->trans('Please complete the "products to display" field.', [], 'Modules.Newproducts.Admin')
                 );
-            } elseif (0 === (int)$productNbr) {
+            } elseif (0 === (int) $productNbr) {
                 $output .= $this->displayError(
-                    $this->trans('Invalid number.', array(), 'Modules.Newproducts.Admin' )
+                    $this->trans('Invalid number.', [], 'Modules.Newproducts.Admin')
                 );
             } else {
-                Configuration::updateValue('PS_NB_DAYS_NEW_PRODUCT', (int)Tools::getValue('PS_NB_DAYS_NEW_PRODUCT'));
-                Configuration::updateValue('NEW_PRODUCTS_NBR', (int)$productNbr);
+                Configuration::updateValue('PS_NB_DAYS_NEW_PRODUCT', (int) Tools::getValue('PS_NB_DAYS_NEW_PRODUCT'));
+                Configuration::updateValue('NEW_PRODUCTS_NBR', (int) $productNbr);
 
                 $this->_clearCache('*');
 
-                $output .= $this->displayConfirmation($this->trans('The settings have been updated.', array(), 'Admin.Notifications.Success'));
+                $output .= $this->displayConfirmation($this->trans('The settings have been updated.', [], 'Admin.Notifications.Success'));
             }
         }
-        return $output.$this->renderForm();
+
+        return $output . $this->renderForm();
     }
 
     public function renderForm()
     {
-        $fields_form = array(
-            'form' => array(
-                'legend' => array(
-                    'title' => $this->trans('Settings', array(), 'Admin.Global'),
-                    'icon' => 'icon-cogs'
-                ),
-                'input' => array(
-                    array(
+        $fields_form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->trans('Settings', [], 'Admin.Global'),
+                    'icon' => 'icon-cogs',
+                ],
+                'input' => [
+                    [
                         'type' => 'text',
-                        'label' => $this->trans('Products to display', array(), 'Modules.Newproducts.Admin'),
+                        'label' => $this->trans('Products to display', [], 'Modules.Newproducts.Admin'),
                         'name' => 'NEW_PRODUCTS_NBR',
                         'class' => 'fixed-width-xs',
-                        'desc' => $this->trans('Define the number of products to be displayed in this block.', array(), 'Modules.Newproducts.Admin'),
-                    ),
-                    array(
-                        'type'  => 'text',
-                        'label' => $this->trans('Number of days for which the product is considered \'new\'', array(), 'Modules.Newproducts.Admin'),
-                        'name'  => 'PS_NB_DAYS_NEW_PRODUCT',
+                        'desc' => $this->trans('Define the number of products to be displayed in this block.', [], 'Modules.Newproducts.Admin'),
+                    ],
+                    [
+                        'type' => 'text',
+                        'label' => $this->trans('Number of days for which the product is considered \'new\'', [], 'Modules.Newproducts.Admin'),
+                        'name' => 'PS_NB_DAYS_NEW_PRODUCT',
                         'class' => 'fixed-width-xs',
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->trans('Save', array(), 'Admin.Actions')
-                ),
-            ),
-        );
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->trans('Save', [], 'Admin.Actions'),
+                ],
+            ],
+        ];
 
-        $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+        $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
 
         $helper = new HelperForm();
         $helper->show_toolbar = false;
-        $helper->table =  $this->table;
+        $helper->table = $this->table;
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $helper->identifier = $this->identifier;
@@ -173,23 +173,23 @@ class Ps_NewProducts extends Module implements WidgetInterface
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) .
             '&configure=' . $this->name .
             '&tab_module=' . $this->tab .
-            '&module_name='.$this->name;
+            '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->tpl_vars = array(
+        $helper->tpl_vars = [
             'fields_value' => $this->getConfigFieldsValues(),
             'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id
-        );
+            'id_language' => $this->context->language->id,
+        ];
 
-        return $helper->generateForm(array($fields_form));
+        return $helper->generateForm([$fields_form]);
     }
 
     public function getConfigFieldsValues()
     {
-        return array(
+        return [
             'PS_NB_DAYS_NEW_PRODUCT' => Tools::getValue('PS_NB_DAYS_NEW_PRODUCT', Configuration::get('PS_NB_DAYS_NEW_PRODUCT')),
             'NEW_PRODUCTS_NBR' => Tools::getValue('NEW_PRODUCTS_NBR', Configuration::get('NEW_PRODUCTS_NBR')),
-        );
+        ];
     }
 
     public function renderWidget($hookName, array $configuration)
@@ -212,11 +212,12 @@ class Ps_NewProducts extends Module implements WidgetInterface
         $products = $this->getNewProducts();
 
         if (!empty($products)) {
-            return array(
+            return [
                 'products' => $products,
                 'allNewProductsLink' => Context::getContext()->link->getPageLink('new-products'),
-            );
+            ];
         }
+
         return false;
     }
 
@@ -226,9 +227,9 @@ class Ps_NewProducts extends Module implements WidgetInterface
 
         if (Configuration::get('PS_NB_DAYS_NEW_PRODUCT')) {
             $newProducts = Product::getNewProducts(
-                (int)$this->context->language->id,
+                (int) $this->context->language->id,
                 0,
-                (int)Configuration::get('NEW_PRODUCTS_NBR')
+                (int) Configuration::get('NEW_PRODUCTS_NBR')
             );
         }
 
@@ -246,7 +247,7 @@ class Ps_NewProducts extends Module implements WidgetInterface
             $this->context->getTranslator()
         );
 
-        $products_for_template = array();
+        $products_for_template = [];
 
         if (is_array($newProducts)) {
             foreach ($newProducts as $rawProduct) {
